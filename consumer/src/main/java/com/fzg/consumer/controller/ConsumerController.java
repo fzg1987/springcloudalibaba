b4f2cce2004b3ca6c5626220eed080ef1d437656
fzg1987 <beginer@yeah.net>
2022-10-20 20:32:52 +0800

    update

--- consumer/src/main/java/com/fzg/consumer/controller/ConsumerController.java
+++ consumer/src/main/java/com/fzg/consumer/controller/ConsumerController.java
@@ -1,22 +1,46 @@
 package com.fzg.consumer.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
+@Slf4j
 public class ConsumerController {
 
     @Autowired
     private DiscoveryClient discoveryClient;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping("/instances")
     public List<ServiceInstance> instances(){
         List<ServiceInstance> provider = this.discoveryClient.getInstances("provider");
         return provider;
     }
+
+    @GetMapping("/index1")
+    public String index1(){
+        // 调用provider服务的index方法
+        // 1. 找到provider实例
+        List<ServiceInstance> list = this.discoveryClient.getInstances("provider");
+        int index = ThreadLocalRandom.current().nextInt(list.size());
+        ServiceInstance serviceInstance = list.get(index);
+        String url = serviceInstance.getUri().toString() + "/index";
+        // 2. 调用
+        log.info("调用的端口是,{}",serviceInstance.getPort());
+        return "调用了端口为" + serviceInstance.getPort() + "的服务，返回结果是：" + this.restTemplate.getForObject(url,String.class);
+    }
+    @GetMapping("/index")
+    public String index(){
+        return this.restTemplate.getForObject("http://provider/index",String.class);
+   }
 }
